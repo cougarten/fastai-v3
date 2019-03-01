39f2fea565833c7c2972134b19f7d15a8c684f31
leon wilmanns <leon@q-garden.de>
2019-03-01 15:19:28 +0100

    Update server.py
    forked to do album covers by decade

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://malea.lacerta.uberspace.de/up/export.pkl'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['20s','30s','40s','50s','60s','70s','80s','90s']
 path = Path(__file__).parent
 
 app = Starlette()
